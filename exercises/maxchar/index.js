8292057ab1b0ad57e886eb33e1f66e57d4bf8330
Marcel Amorim <marcel_amorim@live.com>
2018-03-10 22:52:21 -0300

    Add Maxchar algo

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,39 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar (str) {
+  let obj = {}
+  let max = 0
+  let index = ''
 
-module.exports = maxChar;
+  for (let char of str) {
+    obj[char] = obj[char] + 1 || 1
+    if (obj[char] > max) {
+      max = obj[char]
+      index = char
+    }
+  }
+
+  return index
+}
+
+module.exports = maxChar
+
+// function maxChar (str) {
+//   let obj = {}
+//   let max = 0
+//   let index = ''
+
+//   for (let char of str) {
+//     obj[char] = obj[char] + 1 || 1
+//   }
+
+//   for (let char in obj) {
+//     if (obj[char] > max) {
+//       max = obj[char]
+//       index = char
+//     }
+//   }
+
+//   return index
+// }
