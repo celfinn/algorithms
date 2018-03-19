7cea5164deee81a171491d3b7bac0a5be48be5c8
Marcel Amorim <marcel_amorim@live.com>
2018-03-18 23:17:45 -0300

    Add more tests

--- exercises/pyramid/test.js
+++ exercises/pyramid/test.js
@@ -12,6 +12,12 @@ test('pyramid is a function', () => {
   expect(typeof pyramid).toEqual('function')
 })
 
+test('prints a pryamid for n = 1', () => {
+  pyramid(1)
+  expect(console.log.mock.calls[0][0]).toEqual('#')
+  expect(console.log.mock.calls.length).toEqual(1)
+})
+
 test('prints a pryamid for n = 2', () => {
   pyramid(2)
   expect(console.log.mock.calls[0][0]).toEqual(' # ')
@@ -34,4 +40,14 @@ test('prints a pryamid for n = 4', () => {
   expect(console.log.mock.calls[2][0]).toEqual(' ##### ')
   expect(console.log.mock.calls[3][0]).toEqual('#######')
   expect(console.log.mock.calls.length).toEqual(4)
+})
+
+test('prints a pryamid for n = 5', () => {
+  pyramid(5)
+  expect(console.log.mock.calls[0][0]).toEqual('    #    ')
+  expect(console.log.mock.calls[1][0]).toEqual('   ###   ')
+  expect(console.log.mock.calls[2][0]).toEqual('  #####  ')
+  expect(console.log.mock.calls[3][0]).toEqual(' ####### ')
+  expect(console.log.mock.calls[4][0]).toEqual('#########')
+  expect(console.log.mock.calls.length).toEqual(5)
 })
