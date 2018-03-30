a01e11e731ed843f4056833e90ce67cace2b9cfc
Marcel Amorim <marcel_amorim@live.com>
2018-03-30 19:34:22 -0300

    Vowels and Spiral Matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,165 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix (n) {
+  const result = []
 
-module.exports = matrix;
+  for (let i = 0; i < n; i++) {
+    result.push([])
+  }
+
+  let counter = 1
+  let startRow = 0
+  let endRow = n - 1
+  let startColumn = 0
+  let endColumn = n - 1
+
+  while (startRow <= endRow && startColumn <= endColumn) {
+    for (let i = startColumn; i <= endColumn; i++) {
+      result[startRow][i] = counter
+      counter++
+    }
+
+    startRow++
+
+    for (let i = startRow; i <= endRow; i++) {
+      result[i][endColumn] = counter
+      counter++
+    }
+
+    endColumn--
+
+    for (let i = endColumn; i >= startColumn; i--) {
+      result[endRow][i] = counter
+      counter++
+    }
+
+    endRow--
+
+    for (let i = endRow; i >= startRow; i--) {
+      result[i][startColumn] = counter
+      counter++
+    }
+
+    startColumn++
+  }
+
+  return result
+}
+
+module.exports = matrix
+
+// function notExists (result, row, column) {
+//   const arr = result[row]
+
+//   if (!arr) {
+//     return true
+//   }
+
+//   return !arr[column]
+// }
+
+// function matrix (n) {
+//   const result = []
+//   let row = 0
+//   let column = 0
+//   let direction = 'c+'
+//   const limit = n - 1
+
+//   for (let count = 1; count <= n * n; count++) {
+//     let arr
+
+//     if (!result[row]) {
+//       arr = []
+//       result[row] = arr
+//     } else {
+//       arr = result[row]
+//     }
+
+//     arr[column] = count
+
+//     switch (direction) {
+//       case 'r+':
+//         if (row !== limit && notExists(result, row + 1, column)) {
+//           row++
+//         } else {
+//           column--
+//           direction = 'c-'
+//         }
+//         break
+//       case 'r-':
+//         if (row !== 0 && notExists(result, row - 1, column)) {
+//           row--
+//         } else {
+//           column++
+//           direction = 'c+'
+//         }
+//         break
+//       case 'c+':
+//         if (column !== limit && notExists(result, row, column + 1)) {
+//           column++
+//         } else {
+//           row++
+//           direction = 'r+'
+//         }
+//         break
+//       case 'c-':
+//         if (column !== 0 && notExists(result, row, column - 1)) {
+//           column--
+//         } else {
+//           row--
+//           direction = 'r-'
+//         }
+//         break
+//     }
+//   }
+
+//   return result
+// }
+
+
+// function matrix (n) {
+//   const result = []
+
+//   for (let i = 0; i < n; i++) {
+//     result.push([])
+//   }
+
+//   let counter = 1
+//   let startRow = 0
+//   let endRow = n - 1
+//   let startColumn = 0
+//   let endColumn = n - 1
+
+//   while (startColumn <= endColumn && startRow <= endRow) {
+//     for (let i = startColumn; i <= endColumn; i++) {
+//       result[startRow][i] = counter
+//       counter++
+//     }
+
+//     startRow++
+
+//     for (let i = startRow; i <= endRow; i++) {
+//       result[i][endColumn] = counter
+//       counter++
+//     }
+
+//     endColumn--
+
+//     for (let i = endColumn; i >= startColumn; i--) {
+//       result[endRow][i] = counter
+//       counter++
+//     }
+
+//     endRow--
+
+//     for (let i = endRow; i >= startRow; i--) {
+//       result[i][startColumn] = counter
+//       counter++
+//     }
+
+//     startColumn++
+//   }
+
+//   return result
+// }
