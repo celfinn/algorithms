323e0c5b4116870cec0d77668b545cc5fa9c2386
Marcel Amorim <marcel_amorim@live.com>
2018-03-31 20:51:10 -0300

    Add fib algorithm

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,65 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+const memoize = (fn) => {
+  const cache = {}
 
-module.exports = fib;
+  return (...args) => {
+    if (cache[args]) {
+      return cache[args]
+    }
+
+    const result = fn.apply(this, args)
+    cache[args] = result
+    return result
+  }
+}
+
+let fib = (n) => {
+  if (n < 2) {
+    return n
+  }
+
+  return fib(n - 1) + fib(n - 2)
+}
+
+fib = memoize(fib)
+
+module.exports = fib
+
+// function fib (n, memo = {}) {
+//   if (memo[n]) {
+//     return memo[n]
+//   }
+
+//   if (n < 2) {
+//     memo[n] = n
+//     return n
+//   }
+
+//   memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+//   return memo[n]
+// }
+
+// function fib (n) {
+//   if (n < 2) {
+//     return n
+//   }
+
+//   const result = [0, 1]
+
+//   for (let i = 2; i <= n; i++) {
+//     result.push(result[i - 1] + result[i - 2])
+//   }
+
+//   return result[n]
+// }
+
+
+// function fib (n) {
+//   if (n < 2) {
+//     return n
+//   }
+
+//   return fib(n - 1) + fib(n - 2)
+// }
